5139b3316f3c3da857caac8c74ea44cb85d2da90
Aymeric <aymeric.di-vito@epitech.eu>
2024-01-23 11:15:45 +0100

    fix part1

--- test01/Algo.js
+++ test01/Algo.js
@@ -4,33 +4,40 @@ async function fetchDataAndProcess() {
     try {
         const response = await axios.get('https://apitest.likewatt-infra.com/entry-test/1');
         const data = response.data;
+        console.log('Fetched data:', data);
 
-        const filteredData = data.filter(item => typeof item === 'string' || typeof item === 'number' || typeof item === 'object');
+        const filteredData = filterData(data);
+        const orderedData = sortData(filteredData);
 
-        const orderedData = filteredData.sort((a, b) => {
-            if (typeof a === 'string' && typeof b === 'string') {
-                return a.localeCompare(b);
-            } else if (typeof a === 'number' && typeof b === 'number') {
-                return b - a;
-            } else if (Array.isArray(a) && Array.isArray(b)) {
-                return a.length - b.length;
-            } else if (typeof a === 'object' && typeof b === 'object') {
-                return Object.keys(a).length - Object.keys(b).length;
-            } else {
-                return 0;
-            }
-        });
-
-        const resultObject = { orderedData };
-
-        return resultObject;
+        return { orderedData };
     } catch (error) {
-        console.error('An error occurred while fetching the data:', error.message);
         throw error;
     }
+}
+
+function filterData(data) {
+    return Object.filter(data, item => typeof item === 'string' || typeof item === 'number' || Array.isArray(item) || typeof item === 'object');
+}
+
+function sortData(filteredData) {
+    return Object.keys(filteredData).sort((a, b) => {
+        const typeA = typeof filteredData[a];
+        const typeB = typeof filteredData[b];
+
+        if (typeA === 'string' && typeB === 'string') {
+            return filteredData[a].localeCompare(filteredData[b]);
+        } else if (typeA === 'number' && typeB === 'number') {
+            return filteredData[b] - filteredData[a];
+        } else if (Array.isArray(filteredData[a]) && Array.isArray(filteredData[b])) {
+            return filteredData[a].length - filteredData[b].length;
+        } else if (typeA === 'object' && typeB === 'object') {
+            return Object.keys(filteredData[a]).length - Object.keys(filteredData[b]).length;
+        } else {
+            return 0;
+        }
+    });
 }
 
 fetchDataAndProcess()
     .then(result => console.log('Filtered and ordered data:', result.orderedData))
     .catch(error => console.error('An error occurred:', error.message));
-
